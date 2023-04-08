eb1949f50505dfc6164d33308d34a50f623da504
Ner Karso <nerpkarso@gmail.com>
2023-04-07 21:32:14 -0300

    change name on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="fixed inset-0 grid place-content-center">
       <div className="text-center flex flex-col items-center gap-8">
         <h1 className="text-5xl font-bold">
-          Mantine <span className="text-red-500">&hearts;</span> Tailwind
+          Mantine <span className="text-red-500">&hearts;</span> Tailwind CSS
         </h1>
         <Group>
           <Button>Agree</Button>
