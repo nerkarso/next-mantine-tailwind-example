876890ae59b61eaa8289c1ce76203a6d2c9c1634
Ner Karso <nerpkarso@gmail.com>
2024-06-19 00:18:50 +0000

    ci: remove turbotrace

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    experimental: {
-        turbotrace: {
-            logDetail: true
-        }
-    }
+    output: 'standalone'
 };
 
 module.exports = nextConfig;
