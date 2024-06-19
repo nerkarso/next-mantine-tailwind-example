8eff227908eddd53ad41b9ddf749ccaa096cba67
Ner Karso <nerpkarso@gmail.com>
2024-06-19 00:14:13 +0000

    ci: enable turbotrace

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    turbotrace: {
-        logDetail: true
+    experimental: {
+        turbotrace: {
+            logDetail: true
+        }
     }
 };
 
