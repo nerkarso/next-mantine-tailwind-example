d0d9b76691c2f2bde092afe0b7173dd08e0323d7
Ner Karso <nerpkarso@gmail.com>
2024-07-31 22:01:13 -0300

    optimize package imports

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
   images: {
     remotePatterns: [{ hostname: '*' }],
     unoptimized: true,
