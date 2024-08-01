9319e403235e4262f9c6ebcdc1e9201b20b89b24
Ner Karso <nerpkarso@gmail.com>
2024-07-31 21:59:23 -0300

    turn off Next.js image optimization

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: '*' }],
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
